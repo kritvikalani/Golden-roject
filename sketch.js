68f117ef7c813e0f0a22edee780f01aa3239476f
kritvikalani <67899380+kritvikalani@users.noreply.github.com>
2020-10-03 20:10:35 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,123 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var saviour;
+var saviourImage;
+var corona = []
+var mask1, mask1Image, mask1Group;
+var mask2, mask2Image, mask2Group;
+var score;
+
+function preload() {
+saviourImage= loadImage("ppe.png");
+mask1Image= loadImage("mask.png");
+mask2Image= loadImage("mask2.png");
+}
+
+function setup() {
+  createCanvas(displayWidth,displayHeight);
+  engine = Engine.create();
+    world = engine.world;
+
+    mask1Group = new Group();
+    mask2Group = new Group();
+
+    score = 0;
+
+  saviour= createSprite(displayWidth/2,780,50,50)
+ saviour.addImage(saviourImage);
+
+ if(frameCount % 150 === 0){
+ for(var i=0; i<1; i++){
+  corona.push(new Obstacles(random(0,1200), random(200,1200)));
+ }
+}
+}
+
+function draw() {
+  background(0); 
+  Engine.update(engine);
+
+  if (keyDown(RIGHT_ARROW)) {
+    saviour.velocityX = 3;
+    saviour.velocityY = 0;
+  }
+
+  if (keyDown(LEFT_ARROW)) {
+    saviour.velocityX = -3;
+    saviour.velocityY = 0;
+  }
+
+  if (keyDown(UP_ARROW)) {
+    saviour.velocityY = -3;
+    saviour.velocityX = 0;
+  }
+
+  if (keyDown(DOWN_ARROW)) {
+    saviour.velocityY = 3;
+    saviour.velocityX = 0;
+  }
+
+  if (saviour.isTouching(mask1Group)) {
+    score = score + 1
+   // mask1Group.destroyEch();
+  }
+
+stroke("white");
+textSize(20);  
+fill("white");
+text("Masks collected= " + score, 600, displayHeight/2)
+
+  /*for (var i in corona){
+
+    
+      if(saviour.intersects(corona[i])) {
+      saviour.x = 380;
+     saviour.y = 780;
+    }
+
+  }*/
+  camera.position.x= displayWidth/2
+  camera.position.y= saviour.y
+
+  for(var i=0; i<1; i++){
+  corona[i].display();
+  corona[i].update();
+  }
+  drawSprites();
+  
+Mask1();
+Mask2();
+}
+
+function Mask1() {
+  if (frameCount%200===0) {
+    mask1 = createSprite(random(0,600), saviour.y - 10, 20,20)
+    mask1.addImage(mask1Image);
+    mask1.scale = 0.1;
+    mask1Group.add(mask1)
+  }
+}
+
+function Mask2() {
+  if (frameCount%300===0) {
+    mask2 = createSprite(random(0,600), saviour.y - 10, 20,20)
+    mask2.addImage(mask2Image);
+    mask2.scale = 0.2;
+    mask2Group.add(mask2)
+  }
+}
+
+function intersects(other){
+
+  var d = dist(saviour.x,saviour.y,other.x,other.y)
+  if(d< saviour.x + other.x){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
